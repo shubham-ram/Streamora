75e3dca7dcd7b91288d0e6e2637fca5835e359b1
shubham-ram <shubhamram2k@gmail.com>
2025-12-27 17:44:22 +0530

    feat: added AuthModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma';
+import { AuthModule } from './auth/auth.module';
 
 /**
  * AppModule - Root Module of the Application
@@ -23,7 +24,7 @@ import { PrismaModule } from './prisma';
  * etc.
  */
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
