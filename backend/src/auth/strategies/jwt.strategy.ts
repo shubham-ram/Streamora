8a5f07128f89b2abdb8df816f690d3435f79fd7e
shubham-ram <shubhamram2k@gmail.com>
2026-01-03 00:33:27 +0530

    feat: Implement JWT authentication with current user retrieval

---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET || '',
+    });
+  }
+
+  async validate({ sub }: { sub: string }) {
+    try {
+      const user = await this.prisma.user.findUnique({ where: { id: sub } });
+
+      if (!user) {
+        return null;
+      }
+      return user;
+    } catch {
+      return null;
+    }
+  }
+}
